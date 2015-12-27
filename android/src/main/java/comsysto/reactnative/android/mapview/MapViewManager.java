fe65f62593e34e7a063f697ba42c31e9a9c0fe2a
Draško Kokić <drasko.kokic@gmail.com>
2015-12-27 01:21:16 +0100

    Add RegionChange custom native event

--- android/src/main/java/comsysto/reactnative/android/mapview/MapViewManager.java
+++ android/src/main/java/comsysto/reactnative/android/mapview/MapViewManager.java
@@ -3,15 +3,15 @@ package comsysto.reactnative.android.mapview;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.uimanager.IllegalViewOperationException;
-import com.facebook.react.uimanager.ReactProp;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -31,7 +31,8 @@ public class MapViewManager extends ViewGroupManager<ReactMapView> {
     }
 
     @Override
-    protected ReactMapView createViewInstance(ThemedReactContext reactContext) {
+    protected ReactMapView createViewInstance(final ThemedReactContext reactContext) {
+        Log.d("MapViewManager", "createViewInstance(): ... ");
         mReactMapView = new ReactMapView(reactContext);
         mReactMapView.onCreate(mSavedInstanceState);
         mReactMapView.onResume();
@@ -39,32 +40,28 @@ public class MapViewManager extends ViewGroupManager<ReactMapView> {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 Log.d("MapViewManager", "googleMap = " + googleMap);
+                // Listen to Camera changes
+                googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+                    @Override
+                    public void onCameraChange(CameraPosition cameraPosition) {
+                        Log.d("MapViewManager", "createViewInstance(): OnCameraChangeListener.onCameraChange(): cameraPosition = " + cameraPosition);
+                        WritableMap event = Arguments.createMap();
+                        event.putString("type", "RegionChange");
+                        WritableMap target = Arguments.createMap();
+                        target.putDouble("latitude", cameraPosition.target.latitude);
+                        target.putDouble("longitude", cameraPosition.target.longitude);
+                        event.putMap("target", target);
+                        reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(mReactMapView.getId(), "topChange", event);
+                    }
+                });
                 // Add a marker in Sydney and move the camera
                 LatLng sydney = new LatLng(-34, 151);
                 googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
             }
         });
+
         return mReactMapView;
     }
-/*
-    @ReactMethod
-    public void getMapAsync(final Callback successCallback, Callback errorCallback) {
-        try {
-            mReactMapView.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    successCallback.invoke();
-                }
-            });
-        } catch (IllegalViewOperationException e) {
-            errorCallback.invoke(e.getMessage());
-        }
-    }
 
-    @ReactProp(name="setMinimumWidth")
-    public void setMinimumWidth(ReactMapView view, int minWidth) {
-        view.setMinimumWidth(minWidth);
-    }
-*/
 }
--- index.android.js
+++ index.android.js
@@ -2,46 +2,49 @@
 
 let React = require('react-native')
 
-let {
-  requireNativeComponent,
-  View,
-  Component,
-} = React
 
-
-let AndroidMapViewInterface = {
-  name: 'AndroidMapView',
-  propTypes: {
-    ...View.propTypes,
-  },
-}
-
-let AndroidMapView = requireNativeComponent('AndroidMapView', AndroidMapViewInterface)
-
-
-class MapView extends Component {
+class MapView extends React.Component {
 
   constructor(props) {
     super(props)
+    this._onChange = this._onChange.bind(this)
+  }
+
+  _onChange(event: Event) {
+    console.log("MapView._onChange :", event.nativeEvent)
+    if (event.nativeEvent.type) {
+      if (event.nativeEvent.type === "RegionChange" && this.props.onRegionChange) this.props.onRegionChange(event.nativeEvent)
+    }
   }
 
   componentDidMount () {
+    console.log("MapView.componentDidMount")
   }
 
   componentWillUnmount () {
+    console.log("MapView.componentWillUnmount")
   }
 
   componentWillReceiveProps (nextProps) {
+    console.log("MapView.componentWillReceiveProps :", nextProps)
   }
 
   render () {
     return (
       <AndroidMapView
         { ...this.props }
+        onChange = { this._onChange }
       />
     )
   }
 
 }
+
+MapView.propTypes = {
+  ...React.View.propTypes,
+  onRegionChange: React.PropTypes.func,
+}
+
+let AndroidMapView = React.requireNativeComponent('AndroidMapView', MapView, {nativeOnly: {onChange: true}})
 
 module.exports = MapView
